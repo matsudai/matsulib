238e48da3182211e134c5cb423a070083901d409
matsudai <matsudai.social.1131@gmail.com>
2017-01-04 20:58:20 +0900

    Add has_iterator functor
    イテレータを持っているかどうかを判定するファンクタhas_iteratorを実装した

---
+++ has_iterator.hpp
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <type_traits>
+namespace matsulib
+{
+	namespace _detail
+	{
+		struct IteratorChecker
+		{
+		protected:
+			struct True { static constexpr bool value = true; };
+			struct False { static constexpr bool value = false; };
+
+		protected:
+			template <class _T> static constexpr auto check(typename _T::iterator*)->True;
+			template <class _T> static constexpr auto check(...)->False;
+		};
+	}
+	
+	template <class _T>
+	struct has_iterator : public _detail::IteratorChecker
+	{
+	public:
+		static constexpr auto value = decltype(check <_T>(nullptr))::value;
+	};
+}
