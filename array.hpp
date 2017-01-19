5bb0b4b74be36b83b0d903e3d85e003cdd26f62b
matsudai <matsudai.social.1131@gmail.com>
2017-01-19 12:54:26 +0900

    Create array-class
    std::vectorを継承して最低限の機能のみ実装した

---
+++ array.hpp
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+namespace matsulib
+{
+	template <class _T, class _Allocator> class Array;
+}
+
+#include <vector>
+
+template <class _T, class _Allocator = std::allocator <_T>>
+class matsulib::Array
+	: public std::vector <_T>
+{
+public:
+	Array() = default;
+	Array(const Array &) = default;
+	Array(Array &&) = default;
+	Array &operator =(const Array &) = default;
+	Array &operator =(Array &&) = default;
+	virtual ~Array() = default;
+
+	using parent = std::vector <_T>;
+	using parent::parent;
+};
