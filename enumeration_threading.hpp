b5d15dc549e1c6b38052f61dd66cc4f0835da2ef
__matsudai__ <matsudai.social.1131@gmail.com>
2017-11-21 17:26:47 +0900

    add for_each using thread
    指定したスレッド数でforeachする関数を作った

---
+++ enumeration_threading.hpp
@@ -0,0 +1,69 @@
+﻿#pragma once
+
+#include <vector>
+#include <thread>
+#include <atomic>
+
+namespace matsulib
+{
+  namespace enumerable_threading
+  {
+    template <class Container, class Function>
+    auto for_each(
+      Container &&values,
+      std::size_t number_of_values,
+      unsigned int number_of_thread,
+      Function &&function)
+      -> void
+    {
+      // check number of thread
+      if (number_of_thread <= 0)
+      {
+        number_of_thread = std::max <unsigned int>({ std::thread::hardware_concurrency(), 1 });
+      }
+
+      // create workers
+      std::vector <std::thread> workers;
+      workers.resize(number_of_thread);
+
+      // loop counter
+      std::atomic_size_t task_id = 0;
+
+      // assign work
+      for (auto &worker : workers)
+      {
+        worker = std::thread{ [&]() {
+          for (auto i = task_id.fetch_add(1); i < number_of_values; i = task_id.fetch_add(1))
+          {
+            function(values[i]);
+          }
+        } };
+      }
+
+      // execute parallel
+      for (auto &worker : workers)
+      {
+        worker.join();
+      }
+    }
+
+    template <class Container, class Function>
+    inline auto for_each(Container &&values, unsigned int number_of_thread, Function &&function) -> void
+    {
+      for_each(
+        std::forward <Container>(values),
+        values.size(),
+        number_of_thread,
+        std::forward <Function>(function));
+    }
+
+    template <class Container, class Function>
+    inline auto for_each(Container &&values, Function &&function) -> void
+    {
+      for_each(
+        std::forward <Container>(values),
+        0,
+        std::forward <Function>(function));
+    }
+  }
+}
